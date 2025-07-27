9c849b8199c6c75503f35bca97d35ef8bfb03650
Andrey Mateus <andreymateusrj@hotmail.com>
2025-07-27 09:33:17 -0300

    add: server file

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { routes } from "./routes";
+import { handlingErrorMiddleware } from "./middlewares/handlingErrorMiddleware";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(routes);
+app.use(handlingErrorMiddleware);
+
+app.listen(PORT, () => {
+    console.log(`Server rodando em http://localhost:${PORT}`);
+});
