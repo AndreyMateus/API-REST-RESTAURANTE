d47b30a95449eaa8e3fd1fa23d3ff942da45889e
Andrey Mateus <andreymateusrj@hotmail.com>
2025-07-30 22:21:36 -0300

    ADD: method for create a new product

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -2,6 +2,7 @@ import { ProductRepository } from "../database/types/ProductRepository";
 import { Request, Response, NextFunction } from "express";
 import { knex } from "../database/knex";
 import { AppError } from "../utils/AppError";
+import z from "zod";
 
 export class ProductsController {
     async index(req: Request, res: Response, next: NextFunction) {
@@ -34,5 +35,33 @@ export class ProductsController {
             next(error);
         }
 
+    }
+
+    async create(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { body } = req;
+
+            const nameSchemeValidation = z.string({ required_error: "O Produto deve ter um nome !" })
+                .nonempty("O produto deve um nome com pelo menos uma letra !");
+            const name = nameSchemeValidation.parse(body.name);
+
+            const descriptionSchemeValidation = z.string({ required_error: "O produto deve conter uma descrição !" });
+            const description = descriptionSchemeValidation.parse(body.description);
+
+            const priceSchemeValidation = z.number({ required_error: "O produto deve conter um preço !" }).min(0, "O valor mínimo deve ser maior ou igual a 0!");
+            const price = priceSchemeValidation.parse(body.price);
+
+            await knex<ProductRepository>("products").insert({
+                name,
+                description,
+                price,
+            });
+
+            res.status(201).json({
+                message: "Produto cadastrado com sucesso!"
+            });
+        } catch (error) {
+            next(error);
+        }
     }
 }
