62f7646cc8ebbb2e1f979802f8e811b1a0f4378e
Andrey Mateus <andreymateusrj@hotmail.com>
2025-07-30 22:23:00 -0300

    add: a new route for new method of create a product

--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -8,4 +8,6 @@ const productsController = new ProductsController();
 // TODO: permitir que seja passado um params com chave de name e que possa ser retornado somente um produto com esse NAME ou varios, entao ter tambem um params com nome ALOT, se esse param booleano for TRUE, entao eu retorno muitos com esse nome, caso contrario eu retorno um unico produto com esse name.
 productsRoutes.get("/", productsController.index);
 
-productsRoutes.get("/:id", productsController.show); 
+productsRoutes.get("/:id", productsController.show);
+
+productsRoutes.post("/", productsController.create);
